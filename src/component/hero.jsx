6189855b1c493874d6a462e0cd8ca60c6c77293d
lyril-sly <sylvialyril.sl@gmail.com>
2024-04-09 11:32:28 +0000

    cv

--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -12,7 +12,7 @@ export default function Hero(){
         <h2 className="text-amber-300 text-2xl">Hello, I'm</h2>
         <h1 className="text-6xl text-white font-bold py-5">Sylvia Dugah</h1>
         <h3 className="text-white py-3 ">Web Developer</h3>
-        <button className="" ><Link to="" download className="text-center text-amber-300 bg-slate-700 px-3 py-2 rounded-2xl mt-5">Download Cv</Link></button>
+        <button className="" ><Link to="/CV - Sylvia Lyril.docx.pdf" target="_blank" download className="text-center text-amber-300 bg-slate-700 px-3 py-2 rounded-2xl mt-5">Download Cv</Link></button>
         </div>
         {/* <img className='w-96' src={p1}/> */}
         </div>
